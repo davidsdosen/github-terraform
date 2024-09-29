247f1bc464a5272a5023446876193f441b1d8bd3
davidsdosen <david.sorhaug.dosen@gmail.com>
2024-09-30 01:59:03 +0200

    added sample keyvault

--- backend/main.tf
+++ backend/main.tf
@@ -1,3 +1,6 @@
+# Terraform Configuration
+data "azurerm_client_config" "current" {}
+
 terraform {
   required_providers {
     azurerm = {
@@ -8,14 +11,21 @@ terraform {
 }
 
 provider "azurerm" {
-  # Configuration options
+  features {
+    key_vault {
+      purge_soft_delete_on_destroy    = true
+      recover_soft_deleted_key_vaults = true
+    }
+  }
 }
 
+# Resource Group
 resource "azurerm_resource_group" "rg_backend" {
   name     = var.rg_name
   location = var.location
 }
 
+# Storage Account
 resource "azurerm_storage_account" "sa_backend" {
   name                     = var.sa_name
   resource_group_name      = azurerm_resource_group.rg_backend.name
@@ -24,12 +34,41 @@ resource "azurerm_storage_account" "sa_backend" {
   account_replication_type = "GRS"
 }
 
+# Storage Container
 resource "azurerm_storage_container" "sc_backend" {
   name                  = var.sc_name
   storage_account_name  = azurerm_storage_account.sa_backend.name
   container_access_type = "private"
 }
 
+# Key Vault
+resource "azurerm_key_vault" "example" {
+  name                        = "examplekeyvault"
+  location                    = azurerm_resource_group.example.location
+  resource_group_name         = azurerm_resource_group.example.name
+  enabled_for_disk_encryption = true
+  tenant_id                   = data.azurerm_client_config.current.tenant_id
+  soft_delete_retention_days  = 7
+  purge_protection_enabled    = false
 
+  sku_name = "standard"
+
+  access_policy {
+    tenant_id = data.azurerm_client_config.current.tenant_id
+    object_id = data.azurerm_client_config.current.object_id
+
+    key_permissions = [
+      "Get",
+    ]
+
+    secret_permissions = [
+      "Get",
+    ]
+
+    storage_permissions = [
+      "Get",
+    ]
+  }
+}
 
 
